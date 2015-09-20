58d9a90deb76b4998343a5d7780edd70c93da016
Tyler Neustaedter <rtneust@gmail.com>
2015-09-20 14:47:05 -0500

    updates the javascript file

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,28 @@
 
 window.onload = function() {
 
-	var  urlProfile = "https://api.github.com/users/popesicle",
+	var user = $("input"),
 
-	newProfile = "https://api.github.com/users/",
+ 	urlProfile = "https://api.github.com/users/popesicle";
 
-	newRepos = "https://api.github.com/";
 
 	// dataKey = access_token: apiKey;
 
-
-
+	
 	var gitProfile = {
 		url: urlProfile,
-		// data: {
-		// 		"access_token" : apiKey
-		// 	}
+		success: createProfile,
+		data: {
+				// "access_token" : apiKey
+			}
 		}
 
 	var gitRepos = {
 		url: urlProfile + "/repos",
-		// data: {
-		// 		"access_token" : apiKey
-		// 	}
+		success: createRepos,
+		data: {
+				// "access_token" : apiKey
+			}
 		}
 
 	var createProfile = function(profileData) {
@@ -54,23 +54,26 @@ window.onload = function() {
 		// [repoObj1, repoObj2]
 		var listElement = $("#reposList")[0];
 		repoArr.forEach(function(repObj){
-			listElement.innerHTML += "<div class = repos>" + "<p class = repoName>" + repObj.name + "</p>" + "<p class = starGazers>" + "<i class='fa fa-star'></i> " + repObj.stargazers_count + "<p class = fork>" + "<i class='fa fa-code-fork'></i> " + repObj.forks_count + "</p>" + "<p class = language>" + repObj.language + "</p>" + "<br>" + "<p class = createdOn>" + repObj.created_at + "</p>"
+			listElement.innerHTML += "<div class = repos>" + "<a href=" + repObj.url + " class = repoName>" + repObj.name + "</a>" + "<p class = starGazers>" + "<i class='fa fa-star'></i> " + repObj.stargazers_count + "<p class = fork>" + "<i class='fa fa-code-fork'></i> " + repObj.forks_count + "</p>" + "<p class = language>" + repObj.language + "</p>" + "<br>" + "<p class = createdOn>" + repObj.created_at + "</p>"
 		})		
 	}
 
 
 
+	var startAjax = function() {
+		$.ajax(gitProfile).success(createProfile),
+		$.ajax(gitRepos).success(createRepos);
+	}
 
 
-	var ajaxRepsonse1 = $.ajax(gitProfile).success(createProfile);
+	// var ajaxRepsonse1 = $.ajax(gitProfile).success(createProfile),
+	// 	ajaxRepsonse2 = $.ajax(gitRepos).success(createRepos);
 
-	var ajaxRepsonse2 = $.ajax(gitRepos).success(createRepos);
 
-
-	window.ajaxRepsonse1 = ajaxRepsonse1;
-	console.log("ajax called");
-	window.ajaxRepsonse2 = ajaxRepsonse2;
-	console.log("ajax called again")
-
+	// window.ajaxRepsonse1 = ajaxRepsonse1;
+	// console.log("ajax called");
+	// window.ajaxRepsonse2 = ajaxRepsonse2;
+	// console.log("ajax called again")
+	startAjax()
 
 }
